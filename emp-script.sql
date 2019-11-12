a5459d941df816a975dee5e589ba57b7174a7509
jasvinder <jasvinderlucky.singh@gmail.com>
2019-11-12 21:21:05 +0530

    insert script added

--- emp-script.sql
+++ emp-script.sql
@@ -4,8 +4,7 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
--- -----------------------------------------------------
--- Schema mydb
+-- --------------------------------------------------
 -- -----------------------------------------------------
 -- -----------------------------------------------------
 -- Schema emp_management
